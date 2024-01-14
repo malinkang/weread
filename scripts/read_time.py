8372e4f42ca34af9597d6ba426cd776de537ad33
malinkang <malinkang@adsconflux.com>
2024-01-14 13:55:45 +0800

    add book

--- scripts/read_time.py
+++ scripts/read_time.py
@@ -1,6 +1,7 @@
 import argparse
 from datetime import datetime
 from datetime import timedelta
+import json
 import os
 
 import pendulum
@@ -39,31 +40,47 @@ def insert_to_notion(page_id,timestamp,duration):
         notion_helper.client.pages.create(parent=parent,icon = get_icon("https://www.notion.so/icons/target_red.svg"),properties=properties)
 
 
+def get_file():
+    # 设置文件夹路径
+    folder_path = './OUT_FOLDER'
+
+    # 检查文件夹是否存在
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        entries = os.listdir(folder_path)
+        
+        file_name = entries[0] if entries else None
+        return file_name
+    else:
+        print("OUT_FOLDER does not exist.")
+        return None
+
 if __name__ == "__main__":
     weread_cookie = os.getenv("WEREAD_COOKIE")
     notion_helper = NotionHelper()
     weread_api = WeReadApi()
-    if(os.path.isfile("./OUT_FOLDER/weread.svg")):
+    image_file = get_file()
+    if image_file:
         image_url = notion_helper.image_dict.get("url")
         block_id = notion_helper.image_dict.get("id")
         branch = os.getenv("REF").split("/")[-1]
         repository =  os.getenv("REPOSITORY")
-        new_image_url = f"https://raw.githubusercontent.com/{repository}/{branch}/OUT_FOLDER/weread.svg"
+        new_image_url = f"https://raw.githubusercontent.com/{repository}/{branch}/OUT_FOLDER/{image_file}"
         if(image_url and block_id):
             notion_helper.update_image_block_link(block_id,new_image_url)
     api_data = weread_api.get_api_data()
+    readTimes = {int(key):value for key,value in api_data.get("readTimes").items()}
     now = pendulum.now('Asia/Shanghai').start_of('day')
     today_timestamp = now.int_timestamp
-    if(today_timestamp not in api_data):
-        api_data["readTimes"][str(today_timestamp)] = 0
-    readTimes = dict(sorted(api_data["readTimes"].items()))
+    if(today_timestamp not in readTimes):
+        readTimes[today_timestamp] = 0
+    readTimes = dict(sorted(readTimes.items()))
     results =  notion_helper.query_all(database_id=notion_helper.day_database_id)
     for result in results:
         timestamp = result.get("properties").get("时间戳").get("number")
         duration = result.get("properties").get("时长").get("number")
         id = result.get("id")
-        if(str(timestamp) in readTimes):
-            value = readTimes.pop(str(timestamp))
+        if(timestamp in readTimes):
+            value = readTimes.pop(timestamp)
             if(value !=duration):
                 insert_to_notion(page_id=id,timestamp=timestamp,duration=value)
     for key,value in readTimes.items():
